c9ce9d51462adde0abce5809028d4b32cf572496
VitoCCChen <ccchen@degather.com>
2018-02-01 18:32:13 +0800

    final v1.1

--- app/Http/Controllers/liveInsController.php
+++ app/Http/Controllers/liveInsController.php
@@ -12,14 +12,22 @@ class liveInsController extends Controller
         $curl = new Curl();
         $curl->setHeader('X-Requested-With', 'XMLHttpRequest');
         $curl->setHeader('Content-Type', 'application/json');
-        $curl->get('http://pkfunapi.test/api/getEpisode/'.$id);
+        $curl->get('http://api.pkfun.xyz/api/getEpisode/'.$id);
+
+
 
         if ($curl->error) {
             echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
         } else {
+
             $result = $curl->response;
+            if(count($result->result->data) == 0){
+                return '404 not found!';
+            }
             $data = $result->result->data[0];
-            //dd( compact('results_ep', 'results_pg'));
+
+//            dd( compact('results_ep', 'results_pg'));
+
         }
 
         return view('live_ins.index', compact('data'));
